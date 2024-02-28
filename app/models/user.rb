9f515a3f3b0f4193411b8c6c76c55dd17c66c9db
yuri santos <santosyurial@gmail.com>
2024-02-28 15:58:16 -0300

    Implement validations for the User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  # has_many :products
+  validates :cpf, uniqueness: true
+  validates :address, uniqueness: true
+  validates :username, uniqueness: true, length: { in: 4..20 }
+  validates :cpf, uniqueness: true, length: { is: 11 }, numericality: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
