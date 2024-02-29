b9ae6740f705485aa81167e84002b919f0732959
yuri santos <santosyurial@gmail.com>
2024-02-29 15:41:23 -0300

    Add rentals controller, add devise trackable and implement migration to make trackable work.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_29_145442) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_29_183716) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_29_145442) do
     t.string "address"
     t.string "fullname"
     t.string "cpf"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
