663a31e92b72ec4606399631836454e3ac7bff49
yuri santos <santosyurial@gmail.com>
2024-02-29 14:59:34 -0300

    Create routes for rentals.

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :products do
+    resources :rentals, only: %i[new create]
+  end
 
-  resources :products
   # Defines the root path route ("/")
   # root "posts#index"
 end
