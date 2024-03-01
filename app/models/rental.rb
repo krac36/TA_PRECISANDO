fb47845c83b167fa169412f3cfd6ef9a93d84e97
yuri santos <santosyurial@gmail.com>
2024-03-01 11:20:48 -0300

    Added empty line.

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,6 +2,7 @@ class Rental < ApplicationRecord
   belongs_to :product
   belongs_to :rentee, class_name: "User"
 
+
   validates :product, :rentee, presence: true
   validate :renter_and_rentee_must_be_different,
            :end_date_must_be_after_end_date,
