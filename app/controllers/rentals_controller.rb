68ba326c78c9ca33f4950adc288b22f71917a9af
Roberto Oliveira <rvolive@gmail.com>
2024-03-04 15:56:00 -0300

    added confirmation2

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -7,7 +7,7 @@ class RentalsController < ApplicationController
     @rental.rentee = current_user
     @rental.confirmed = false
     if @rental.save
-      redirect_to products_path, notice: "Rental Request successfuly sent, please await owner approval."
+      redirect_to products_path, notice: "Rental Request successfully sent, please await owner approval."
     else
       render "products/show", status: :unprocessable_entity
     end
